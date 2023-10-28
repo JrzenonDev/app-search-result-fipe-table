6dd7b01c3a79a10802bac5f7e68a054799677915
JrzenonDev <jrobertoonb@gmail.com>
2023-10-28 11:57:52 -0300

    feat: create component SelectAutoComplete

---
+++ src/app/components/Common/SelectAutoComplete/style.ts
@@ -0,0 +1,6 @@
+import { Autocomplete } from "@mui/material";
+import { styled } from "@mui/material/styles";
+
+export const StyledAutoComplete = styled(Autocomplete)`
+  margin-bottom: ${({ theme }) => theme.spacing(1)};
+`;
